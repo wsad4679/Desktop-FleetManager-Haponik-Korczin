385c961e97d3437941dbabe3762e36f481db03f4
Szymon <szymon.haponik@cyfroweszkoly.pl>
2026-03-13 19:24:18 +0100

    [ADD] IVehicleRepository interface

--- FleetManager/Services/IVehicleRepository.cs
+++ FleetManager/Services/IVehicleRepository.cs
@@ -1,6 +1,11 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using FleetManager.Models;
+
 namespace FleetManager.Services;
 
-public interface IVehicleRepository
+public interface IVehicleRepository // interfejs definiuje jak klasa będzie wczytywać dane z json
 {
-    
+    Task<List<Vehicle>> LoadVehiclesAsync(); // asynchroniczna metoda wczytująca dane z Json do listy z danymi typi Vehicle
+    Task SaveVehicleAsync(List<Vehicle> vehicles); // asynchroniczna metoda zapisująca listę z Vehicle do Json
 }
