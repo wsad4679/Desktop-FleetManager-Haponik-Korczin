3c32c9d3878a7367508b6ae9d9599cae0c69af45
szymonh <szymon.haponik@cyfroweszkoly.pl>
2026-03-19 11:10:17 +0100

    [CREATE] user control item view

--- FleetManager/Models/Vehicle.cs
+++ FleetManager/Models/Vehicle.cs
@@ -13,7 +13,7 @@ public enum VehicleStatus // enum aby wyeliminować możliwe błędy przy zmieni
 
 public class Vehicle: ReactiveObject // dziedziczy aby ułatwić informowanie o zmianie obiektu i wprowadzić zmianę w wyglądzie
 {
-    [Reactive] public string ViehicleName { get; set; } = string.Empty; // [Reactive] - skrócenie kodu za pomocą Foody, do informowania wyglądu o zmianie
+    [Reactive] public string VehicleName { get; set; } = string.Empty; // [Reactive] - skrócenie kodu za pomocą Foody, do informowania wyglądu o zmianie
     [Reactive] public string RegistrationNumber { get; set; } = string.Empty; // string.Empty po to aby ustawić pusty tekst, żeby UI i MVVM nie robiło problemów z nullem
     [Reactive] public int FuelLevel { get; set; }
     [Reactive] public VehicleStatus Status { get; set; } = VehicleStatus.Available; // do enum odwołojemy się jak do obiektu 
