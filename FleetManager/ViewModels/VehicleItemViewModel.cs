59148930d4188ffbb050d6c2d65f6fac08c3e8f1
szymonh <szymon.haponik@cyfroweszkoly.pl>
2026-03-19 11:25:31 +0100

    [CREATE] vehicle item view model

--- FleetManager/ViewModels/VehicleItemViewModel.cs
+++ FleetManager/ViewModels/VehicleItemViewModel.cs
@@ -1,7 +1,20 @@
 // (ViewModel dla UserControl)
+
+using System;
+using FleetManager.Models;
+using ReactiveUI;
+
 namespace FleetManager.ViewModels;
 
 public class VehicleItemViewModel : ViewModelBase
 {
+    public Vehicle Vehicle { get; }
+
+    public VehicleItemViewModel(Vehicle vehicle)
+    {
+        Vehicle = vehicle;
+        
+    }
+    
     
 }
